e2918af8856428fcd88803aeb944fbc140e919b7
Till-B <bergmann@uni-potsdam.de>
2021-04-01 11:44:42 +0200

    ColorMode Activated Co-authored-by: Antony <DuckNrOne@users.noreply.github.com>

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -6,38 +6,46 @@ import {
   MenuList,
   MenuItem,
   MenuButton,
-  IconButton,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  IconButton,
+  ColorModeScript,
+  useColorMode,
 } from "@chakra-ui/react"
-import { HamburgerIcon } from "@chakra-ui/icons"
+import { HamburgerIcon, SunIcon } from "@chakra-ui/icons"
 import { Head, useMutation } from "blitz"
 import logout from "app/auth/mutations/logout"
 
 export default function Layout({ children }) {
   const [logoutMutation] = useMutation(logout)
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <>
       <Head>
         <title>{"Farmers' Market"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ColorModeScript initialColorMode="light" />
       <Flex direction="column" alignItems="center">
-        <Heading fontSize="2xl" marginBottom="1rem">
+        <Heading
+          fontSize="2xl"
+          marginBottom="1rem"
+          bgClip="text"
+          bgGradient="linear(to-r, green.600, green.400, yellow.400, yellow.600)"
+        >
           Farmers' Market
         </Heading>
-        <Flex
-          style={{ width: "100vw" > "850px" ? "100vw" : "850px" }}
-          grow={1}
-          direction="column"
-          justifyContent="space-between"
-        >
+        <Flex width={["100vw", "850px"]} grow={1} direction="column" justifyContent="space-between">
           <Flex
             borderWidth="0.2rem"
             borderRadius="lg"
             direction="row"
             justifyContent="space-between"
+            paddingY="0.5rem"
+            margin="1rem"
+            paddingY="0.5rem"
+            margin="1rem"
           >
             <Flex>
               <Breadcrumb separator="/">
@@ -73,6 +81,15 @@ export default function Layout({ children }) {
                 <Link href="/archivedLists">
                   <MenuItem>Archived Lists</MenuItem>
                 </Link>
+
+                <MenuItem>
+                  {" "}
+                  <IconButton
+                    aria-label="light-mode"
+                    icon={<SunIcon />}
+                    onClick={toggleColorMode}
+                  />
+                </MenuItem>
               </MenuList>
             </Menu>
           </Flex>
