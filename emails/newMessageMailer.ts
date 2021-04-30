65b375728225122fd7383a0983fb34aa00485417
DuckNrOne <tony.kamp@web.de>
2021-04-30 12:08:06 +0200

    added from mail to forgotPassword- and newMessageMailer

--- emails/newMessageMailer.ts
+++ emails/newMessageMailer.ts
@@ -22,6 +22,7 @@ export async function newMessageMailer({
     template: "newMessage",
     message: {
       to: to,
+      from: process.env.MAIL_MAIL,
       attachments: [
         {
           filename: "entel.png",
